78e701b345cf625298e15e8568440e9bd75d843c
小阿熊 <786507017@qq.com>
2025-06-25 18:06:56 +0800

    feat: update nvim plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local path = vim.fn.argv()[1]
+    if path and vim.fn.isdirectory(path) == 1 then
+      vim.cmd("cd " .. path)
+    end
+  end,
+})
